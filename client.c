255caf89f34901fc6771c9d706925954b22e2efd
kikuke <kkkjs0814@naver.com>
2022-10-02 15:26:23 +0900

    report

--- client.c
+++ client.c
@@ -4,97 +4,98 @@
 #include <windows.h>
 #include <process.h>
 
-#define BUF_SIZE 100
-#define SERVER_IP "127.0.0.1"
-#define SERVER_PORT 2388
-#define MAP_SIZE 20
+#define BUF_SIZE 100 //메시지를 받을 곳 크기 100
+#define SERVER_IP "127.0.0.1" //연결할 게임 서버의 IP지정
+#define SERVER_PORT 2388 //연결할 게임 서버의 PORT지정
+#define MAP_SIZE 20 //게임 맵의 크기
 
-unsigned WINAPI SendMsg(void* arg);
-unsigned WINAPI RecvMsg(void* arg);
-void ErrorHandling(char* msg);
-void DrawGame();
-void PlayerMove(char* buf);
+unsigned WINAPI SendMsg(void* arg); //게임 클라이언트 메시지 전송 스레드 함수
+unsigned WINAPI RecvMsg(void* arg); //서버 메시지 수신 스레드 함수
+void ErrorHandling(char* msg); //에러 체크함수
+void DrawGame(); //게임 화면 그리기 함수
+void PlayerMove(char* buf); //서버 수신 데이터를 이용해 플레이어 위치를 움직여주는 함수
 
-char msg[BUF_SIZE];
-char map[MAP_SIZE][MAP_SIZE];
-int isGameStart=0;
-int my_Playnum;
-int player[2][2];//위치
+char msg[BUF_SIZE]; //게임메시지 전송버퍼
+char map[MAP_SIZE][MAP_SIZE]; //맵 표현 배열
+int isGameStart=0; //0이면 게임준비중, 1이면 게임시작
+int my_Playnum; //서버에게 부여받을 플레이어 번호
+int player[2][2]; //p1, p2의 맵상 위치
 
 int main(void)
 {
-	WSADATA wsaData;
-	SOCKET hSock;
-	SOCKADDR_IN servAdr;
-	HANDLE hSndThread, hRcvThread;
+	WSADATA wsaData; //윈속 초기화 정보 전달받을 자료구조
+	SOCKET hSock; //운영체제에서 할당해 준 소켓 파일 디스크립터를 받을 곳
+	SOCKADDR_IN servAdr; //접속할 서버에 관련된 데이터
+	HANDLE hSndThread, hRcvThread; //운영체제에서 할당해 준 핸들 번호 받을 곳
 
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-		ErrorHandling("WSAStartup() error!");
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)//윈속 2.2버전으로 초기화후 그 정보 wsaData에 저장
+		ErrorHandling("WSAStartup() error!");//에러메시지 출력
 
-	hSock = socket(PF_INET, SOCK_STREAM, 0);
+	hSock = socket(PF_INET, SOCK_STREAM, 0);//운영체제에 IPV4 프로토콜로, 스트림소켓 유형중 0번(TCP)소켓을 할당해달라고 요청한 후 파일 디스크립터를 받아옴
 
-	memset(&servAdr, 0, sizeof(servAdr));
-	servAdr.sin_family = AF_INET;
-	servAdr.sin_addr.s_addr = inet_addr(SERVER_IP);
-	servAdr.sin_port = htons(SERVER_PORT);
+	memset(&servAdr, 0, sizeof(servAdr));//서버 관련 데이터를 받아올 자료구조 전체 비트를 0으로 세팅
+	servAdr.sin_family = AF_INET;//인터넷 프로토콜 주소체계로 값 할당. 인터넷 프로토콜을 이용하겠다.
+	servAdr.sin_addr.s_addr = inet_addr(SERVER_IP);//문자열을 주소유형으로 변환. ip값 저장
+	servAdr.sin_port = htons(SERVER_PORT);//포트 번호를 네트워크 전송을 위해 빅엔디안으로 변환.
 
-	if(connect(hSock, (SOCKADDR*)&servAdr, sizeof(servAdr))==SOCKET_ERROR)
+	if(connect(hSock, (SOCKADDR*)&servAdr, sizeof(servAdr))==SOCKET_ERROR)//hSock소켓을 이용해 servAdr설정값으로 연결. 이때 함수 양식에 맞춰 (SOCKADDR*)로 형변환. 오류시 에러메시지 출력
 		ErrorHandling("connect() error!");
 
-	hSndThread = (HANDLE)_beginthreadex(NULL, 0, SendMsg, (void*)&hSock, 0, NULL);
-	hRcvThread = (HANDLE)_beginthreadex(NULL, 0, RecvMsg, (void*)&hSock, 0, NULL);
+	puts("20192388 김진수");
+	hSndThread = (HANDLE)_beginthreadex(NULL, 0, SendMsg, (void*)&hSock, 0, NULL);//SendMsg스레드를 실행후 핸들 값을 받아오기. 전달 인자로 이용중인 소켓 전달
+	hRcvThread = (HANDLE)_beginthreadex(NULL, 0, RecvMsg, (void*)&hSock, 0, NULL);//RecvMsg스레드를 실행후 핸들 값을 받아오기. 전달 인자로 이용중인 소켓 전달
 
-	WaitForSingleObject(hSndThread, INFINITE);
-	WaitForSingleObject(hRcvThread, INFINITE);
-	closesocket(hSock);
-	WSACleanup();
+	WaitForSingleObject(hSndThread, INFINITE);//스레드가 끝날 때까지 무한히 대기
+	WaitForSingleObject(hRcvThread, INFINITE);//스레드가 끝날 때까지 무한히 대기
+	closesocket(hSock);//소켓 닫기 요청
+	WSACleanup();//윈속 초기화
 	return 0;
 }
 
-unsigned WINAPI SendMsg(void* arg)
+unsigned WINAPI SendMsg(void* arg)//메시지 전송함수
 {
-	SOCKET hSock = *((SOCKET*)arg);
+	SOCKET hSock = *((SOCKET*)arg);//보이드 포인터 인자로 받았던 소켓 다시 SOCKET으로 되돌리기
 	while (TRUE)
 	{
-		msg[0] = _getch(); //입력없이
-		msg[1] = my_Playnum;
-		msg[2] = '\n';
+		msg[0] = _getch(); //에코없이 키보드 값 하나 받아서 서버로 전송할 메시지 첫번째 비트에 할당
+		msg[1] = my_Playnum; //서버로 전송할 메시지 두번째 비트에 현재 클라이언트의 플레이어넘버 정보 넣기
+		msg[2] = '\n'; //메시지 끝났음을 표현
 
-		send(hSock, msg, 3, 0);
-		Sleep(300);
+		send(hSock, msg, 3, 0);//hSock을 이용해 3비트 메시지 전송
+		Sleep(300);//과한 입력처리 방지를 위한 슬립
 	}
 	return 0;
 }
 
-unsigned WINAPI RecvMsg(void* arg)
+unsigned WINAPI RecvMsg(void* arg)//메시지 수신함수
 {
-	int hSock = *((SOCKET*)arg);
-	char buf[BUF_SIZE];
-	int strLen;
+	int hSock = *((SOCKET*)arg);//보이드 포인터 인자로 받았던 소켓 다시 SOCKET으로 되돌리기
+	char buf[BUF_SIZE];//송신버퍼와 데이터 꼬임 방지를 위한 수신전용 버퍼
+	int strLen;//메시지 길이 저장
 	while (TRUE)
 	{
-		strLen = recv(hSock, buf, sizeof(buf)-1, 0);
-		if (strLen == -1)
+		strLen = recv(hSock, buf, sizeof(buf)-1, 0); //메시지 수신 후 메시지 길이 저장. 
+		if (strLen == -1) //비정상 종료
 			return -1;
 
-		if (buf[0] == 1 || buf[0] == 2)//플레이어 넘버 초기화
+		if (buf[0] == 1 || buf[0] == 2)//서버 데이터에서 전달받은 첫번째 비트 값이 1또는 2일경우
 		{
-			my_Playnum = buf[0];
+			my_Playnum = buf[0];//전달받은 넘버로 플레이어 넘버 초기화
 			printf("Your PlayerNum: %d\n", my_Playnum);
 		}
 
-		if (strstr(buf, "Start") != NULL)
+		if (strstr(buf, "Start") != NULL)//만약 전달받은 문자열에 Start가 섞여있을 경우 게임 시작. tcp는 스트림 성격이기에 데이터가 서버에서 보낸 패킷수보다 패킷을 합쳐서 더 작게 보낼수 있기에 strstr로함.
 		{
-			Sleep(1000);
+			Sleep(1000);//이전 정보를 보여주기 위해 잠시 슬립
 
-			isGameStart = 1;
+			isGameStart = 1;//게임 스타트 플래그 set
 			player[0][0] = 0 + 1;//플레이어1의 x 초기 위치
 			player[0][1] = MAP_SIZE / 2 + 1;//플레이어1의 y 초기 위치
 
 			player[1][0] = MAP_SIZE - 2;//플레이어2의 x 초기 위치
 			player[1][1] = MAP_SIZE / 2 - 1;//플레이어2의 y 초기 위치
 
-			for (int i = 0; i < MAP_SIZE; i++)
+			for (int i = 0; i < MAP_SIZE; i++)//게임 맵 데이터 세팅
 				for (int j = 0; j < MAP_SIZE; j++)
 				{
 					if (i == 0 || i == MAP_SIZE - 1 || j == 0 || j == MAP_SIZE - 1)
@@ -103,37 +104,37 @@ unsigned WINAPI RecvMsg(void* arg)
 						map[i][j] = ' ';
 				}
 
-			map[player[0][1]][player[0][0]] = '@';
-			map[player[1][1]][player[1][0]] = '@';
-			DrawGame();
+			map[player[0][1]][player[0][0]] = '@';//초기 플레이어 위치 표시
+			map[player[1][1]][player[1][0]] = '@';//초기 플레이어 위치 표시
+			DrawGame();//설정된 맵을 그려줌
 		}
 
-		if (isGameStart)
+		if (isGameStart)//게임 시작 플래그가 설정되어있을경우
 		{
-			PlayerMove(buf);
-			DrawGame();
+			PlayerMove(buf);//수신 값을 참고해 플레이어를 맵위에서 움직임
+			DrawGame();//위에서 변경된 맵을 다시 그림
 		}
 
-		buf[strLen] = 0;
+		buf[strLen] = 0;//문자열 끝 표시
 
 		//나중에 주석처리하기
-		fputs(buf, stdout);
+		fputs(buf, stdout);//받은 문자열(버퍼) 출력
 	}
 	return 0;
 }
 
-void PlayerMove(char* buf)
+void PlayerMove(char* buf)//버퍼값에 따라 플레이어를 움직임
 {
 	if (buf[0] == 'w')//위로 움직이기
 	{
-		if (player[buf[1] - 1][1] - 1 > 0)
+		if (player[buf[1] - 1][1] - 1 > 0)//버퍼값의 두번째 비트에 어떤 플레이어가 메시지를 보냈는지 정보가 있는데 이를 이용해 플레이어를 움직임. 이때 해당 플레이어가 맵을 벗어나게되는지 검사.
 		{
-			map[player[buf[1] - 1][1]][player[buf[1] - 1][0]] = ' ';
-			player[buf[1] - 1][1] -= 1;
-			map[player[buf[1] - 1][1]][player[buf[1] - 1][0]] = '@';
+			map[player[buf[1] - 1][1]][player[buf[1] - 1][0]] = ' ';//움직일 플레이어의 현재 위치를 공백으로 바꿔줌
+			player[buf[1] - 1][1] -= 1;//플레이어의 위치를 위로 이동
+			map[player[buf[1] - 1][1]][player[buf[1] - 1][0]] = '@';//움직인 플레이어의 위치를 맵에 다시 그려줌
 		}
 	}
-	else if (buf[0] == 's')
+	else if (buf[0] == 's')//아래로 움직이기 w일때와 작동 방식은 같음.
 	{
 		if (player[buf[1] - 1][1] + 1 < MAP_SIZE - 1)
 		{
@@ -142,30 +143,31 @@ void PlayerMove(char* buf)
 			map[player[buf[1] - 1][1]][player[buf[1] - 1][0]] = '@';
 		}
 	}
-	else if (buf[0] == ' ')
+	else if (buf[0] == ' ')//총알 발사하기 어떤 플레이어가 발사했느냐에 따라 처리가 조금씩 다름
 	{
-		if (buf[1] == 1)
+		if (buf[1] == 1)//1번 플레이어가 발사했을 경우
 		{
-			for (int i = player[buf[1] - 1][0] + 1; i < MAP_SIZE - 1; i++)
+			for (int i = player[buf[1] - 1][0] + 1; i < MAP_SIZE - 1; i++)//1번 플레이어의 바로 오른쪽 부터 오른쪽 맵 끝까지 검사 및 처리
 			{
-				if (map[player[buf[1] - 1][1]][i] == '@')
+				if (map[player[buf[1] - 1][1]][i] == '@')//상대 플레이어가 해당 범위에 있을 경우
 				{
-					map[player[buf[1] - 1][1]][i] = '!';
-					DrawGame();
-					Sleep(2000);
+					map[player[buf[1] - 1][1]][i] = '!';//상대 플레이어의 표시를 !로 바꿈
+					DrawGame();//맵을 다시 그림
+					Sleep(2000);//2초대기
 
-					system("cls");
-					puts("Player1 Win!");
+					system("cls");//화면 지우기
+					puts("20192388 김진수");
+					puts("Player1 Win!");//플레이어1 승리창
 					Sleep(10000);
 				}
 
-				map[player[buf[1] - 1][1]][i] = '*';
+				map[player[buf[1] - 1][1]][i] = '*';//해당 범위를 전부 *로 표시
 			}
 		}
-		else if(buf[1] == 2)
+		else if(buf[1] == 2)//2번 플레이어가 발사했을 경우
 		{
-			for (int i = player[buf[1] - 1][0] - 1; i > 0; i--)
-			{
+			for (int i = player[buf[1] - 1][0] - 1; i > 0; i--)//2번 플레이어의 바로 왼쪽 부터 왼쪽 맵 끝까지 검사 및 처리
+			{//작동 방식은 플레이어1과 동일
 				if (map[player[buf[1] - 1][1]][i] == '@')
 				{
 					map[player[buf[1] - 1][1]][i] = '!';
@@ -173,6 +175,7 @@ void PlayerMove(char* buf)
 					Sleep(2000);
 
 					system("cls");
+					puts("20192388 김진수");
 					puts("Player2 Win!");
 					Sleep(10000);
 				}
@@ -183,26 +186,26 @@ void PlayerMove(char* buf)
 	}
 }
 
-void DrawGame()
+void DrawGame()//맵 그리기 함수
 {
-	system("cls");
-
-	puts("Move: W, S; Shot: Space;");
-	for (int i = 0; i < MAP_SIZE; i++)
+	system("cls");//화면 초기화
+	puts("20192388 김진수");
+	puts("Move: W, S; Shot: Space;");//게임 조작키 표시
+	for (int i = 0; i < MAP_SIZE; i++)//맵 전체를 돌며 해당 정보를 화면에 표시
 	{
 		for (int j = 0; j < MAP_SIZE; j++)
 			putc(map[i][j], stdout);
 		putc('\n', stdout);
 	}
-	for (int i = player[0][0] + 1; i < MAP_SIZE - 1; i++)
+	for (int i = player[0][0] + 1; i < MAP_SIZE - 1; i++)//만약 1번 플레이어 직선상에 총알 발사된 흔적이 있었다면 지우기
 		if(map[player[0][1]][i] == '*')
 			map[player[0][1]][i] = ' ';
-	for (int i = player[1][0] - 1; i > 0; i--)
+	for (int i = player[1][0] - 1; i > 0; i--)//만약 2번 플레이어 직선상에 총알 발사된 흔적이 있었다면 지우기
 		if (map[player[0][1]][i] == '*')
 			map[player[1][1]][i] = ' ';
 }
 
-void ErrorHandling(char* msg)
+void ErrorHandling(char* msg)//에러 표시 함수
 {
 	fputs(msg, stderr);
 	fputc('\n', stderr);
--- server.c
+++ server.c
@@ -4,88 +4,89 @@
 #include <windows.h>
 #include <process.h>
 
-#define BUF_SIZE 100
-#define MAX_CLNT 2
-#define SERVER_PORT 2388
+#define BUF_SIZE 100 //메시지를 받을 곳 크기 100
+#define MAX_CLNT 2 //최대 수용 플레이어
+#define SERVER_PORT 2388 //게임 서버의 PORT지정
 
-unsigned WINAPI HandleClnt(void* arg);
-void SendMsg(char* msg, int len);
-void ErrorHandling(char* msg);
+unsigned WINAPI HandleClnt(void* arg); //각 클라이언트의 처리 스레드 함수
+void SendMsg(char* msg, int len); //메시지 전송 함수
+void ErrorHandling(char* msg); //에러처리 함수
 
-int clntCnt = 0;
-SOCKET clntSocks[MAX_CLNT];
-HANDLE hMutex;
+int clntCnt = 0; //현재 클라이언트 카운트
+SOCKET clntSocks[MAX_CLNT]; //최대 클라이언트 수 만큼 소켓 파일 디스크립터 저장 배열
+HANDLE hMutex; //동기화를 위한 뮤텍스
 
 int main()
 {
-	WSADATA wsaData;
-	SOCKET hServSock, hClntSock;
-	SOCKADDR_IN servAdr, clntAdr;
-	int clntAdrSz;
-	HANDLE hThred;
+	WSADATA wsaData; //윈속 초기화 정보 전달받을 자료구조
+	SOCKET hServSock, hClntSock; //운영체제에서 할당해 준 소켓 파일 디스크립터를 받을 곳 각 서버, 클라이언트임.
+	SOCKADDR_IN servAdr, clntAdr; //생성할 서버에 관련된 데이터를 담을곳/수신받은 클라이언트 정보에 관한 데이터를 담을 곳
+	int clntAdrSz;//클라이언트 정보 사이즈
+	HANDLE hThred;//스레드 핸들 정보 저장용
 
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-		ErrorHandling("WSAStartup() error!");
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)//윈속 2.2버전으로 초기화후 그 정보 wsaData에 저장
+		ErrorHandling("WSAStartup() error!");//에러메시지 출력
 
-	hMutex = CreateMutex(NULL, FALSE, NULL);
-	hServSock = socket(PF_INET, SOCK_STREAM, 0);
+	hMutex = CreateMutex(NULL, FALSE, NULL);//동기화를 위한 뮤텍스 생성
+	hServSock = socket(PF_INET, SOCK_STREAM, 0);//운영체제에 IPV4 프로토콜로, 스트림소켓 유형중 0번(TCP)소켓을 할당해달라고 요청한 후 파일 디스크립터를 받아옴
 
-	memset(&servAdr, 0, sizeof(servAdr));
-	servAdr.sin_family = AF_INET;
-	servAdr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servAdr.sin_port = htons(SERVER_PORT);
+	memset(&servAdr, 0, sizeof(servAdr));//서버용 소켓 생성 데이터를 저장할 자료구조 전체 비트를 0으로 세팅
+	servAdr.sin_family = AF_INET;//인터넷 프로토콜 주소체계로 값 할당. 인터넷 프로토콜을 이용하겠다.
+	servAdr.sin_addr.s_addr = htonl(INADDR_ANY);//자신에게 온 어떤 NIC의 IP든 허용
+	servAdr.sin_port = htons(SERVER_PORT);//리스너 포트 번호를 네트워크 전송을 위해 빅엔디안으로 변환.
 
-	if (bind(hServSock, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR)
+	if (bind(hServSock, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR) //해당 소켓에 위에서 servAdr의 정보(ip, 포트 등)를 입력
 		ErrorHandling("bind() error");
-	if (listen(hServSock, 2) == SOCKET_ERROR)
+	if (listen(hServSock, 2) == SOCKET_ERROR) //최대 서버 대기인원 2명
 		ErrorHandling("listen() error");
 
+	puts("20192388 김진수");
 	while (TRUE)
 	{
-		if (clntCnt < MAX_CLNT)
+		if (clntCnt < MAX_CLNT)//현재 클라이언트가 최대인원이 아닐경우 연결허용
 		{
-			clntAdrSz = sizeof(clntAdr);
-			hClntSock = accept(hServSock, (SOCKADDR*)&clntAdr, &clntAdrSz);
+			clntAdrSz = sizeof(clntAdr);//accept에 주소크기를 저장한 데이터를 넘길 변수
+			hClntSock = accept(hServSock, (SOCKADDR*)&clntAdr, &clntAdrSz);//해당 소켓에 접속한 클라이언트의 연결을 허용, 새로운 소켓 부여
 
-			WaitForSingleObject(hMutex, INFINITE);
-			clntSocks[clntCnt++] = hClntSock;
-			ReleaseMutex(hMutex);
+			WaitForSingleObject(hMutex, INFINITE);//소켓 목록 동기화를 위한 뮤텍스 대기
+			clntSocks[clntCnt++] = hClntSock;//새로 생성된 소켓 등록
+			ReleaseMutex(hMutex);//뮤텍스 이용 종료
 
-			hThred = (HANDLE)_beginthreadex(NULL, 0, HandleClnt, (void*)&hClntSock, 0, NULL);
-			printf("Connected client IP: %s\n", inet_ntoa(clntAdr.sin_addr));
+			hThred = (HANDLE)_beginthreadex(NULL, 0, HandleClnt, (void*)&hClntSock, 0, NULL);//새로 만들어진 소켓을 인자로 넘기며 해당 클라이언트의 처리 스레드 시작
+			printf("Connected client IP: %s\n", inet_ntoa(clntAdr.sin_addr));//클라이언트 ip 표시
 		}
 	}
-	closesocket(hServSock);
-	WSACleanup();
-	return 0;
+	closesocket(hServSock);//소켓 종료
+	WSACleanup();//
+	return 0;//윈속 초기화
 }
 
 unsigned WINAPI HandleClnt(void* arg)
 {
-	SOCKET hClntSock = *((SOCKET*)arg);
-	int strLen = 0, i;
-	char msg[BUF_SIZE];
+	SOCKET hClntSock = *((SOCKET*)arg);//보이드포인터로 받은 소켓 파일디스크립터 복구
+	int strLen = 0, i;//수신데이터 크기 저장용, 루프용
+	char msg[BUF_SIZE];//각 클라이언트별 수신/전송버퍼
 
-	WaitForSingleObject(hMutex, INFINITE);
-	msg[0] = clntCnt; msg[1] = '\n';
-	send(clntSocks[clntCnt - 1], msg, 2, 0);//플레이어 번호 부여
-	ReleaseMutex(hMutex);
+	WaitForSingleObject(hMutex, INFINITE);//플레이어 넘버 동기화를 위한 뮤텍스 대기
+	msg[0] = clntCnt; msg[1] = '\0';//현재 클라이언트 수를 이용한 플레이어를 전송할 메시지 첫번째 비트에 추가. 그 다음 비트는 메시지 종료를 의미
+	send(clntSocks[clntCnt - 1], msg, 2, 0);//접속한 대상에게 부여할 플레이어 넘버를 전송.
+	ReleaseMutex(hMutex);//뮤텍스 이용 종료
 
 	if (clntCnt == MAX_CLNT)//2명 모두 왔을경우 스타트
 	{
-		SendMsg("Ready 3\n", sizeof("Ready 3\n"));
+		SendMsg("Ready 3\n", sizeof("Ready 3\n"));//대기 안내 메시지
 		Sleep(1000);
-		SendMsg("Ready 2\n", sizeof("Ready 3\n"));
+		SendMsg("Ready 2\n", sizeof("Ready 3\n"));//대기 안내 메시지
 		Sleep(1000);
-		SendMsg("Ready 1\n", sizeof("Ready 3\n"));
+		SendMsg("Ready 1\n", sizeof("Ready 3\n"));//대기 안내 메시지
 		Sleep(1000);
-		SendMsg("Start", sizeof("Start"));
+		SendMsg("Start", sizeof("Start"));//Start메시지 전송. 이 메시지를 수신한 클라이언트들은 게임이 시작됨을 알게되어 관련 처리를 하게됨.
 	}
-	while ((strLen = recv(hClntSock, msg, sizeof(msg), 0)) != 0)
+	while ((strLen = recv(hClntSock, msg, sizeof(msg), 0)) != 0)//이후 클라이언트들이 전송하는 메시지들을 에코로 방송해줌
 		SendMsg(msg, strLen);
 
-	WaitForSingleObject(hMutex, INFINITE);
-	for (i = 0; i < clntCnt; i++)
+	WaitForSingleObject(hMutex, INFINITE);//소켓 목록 동기화를 위한 뮤텍스 대기
+	for (i = 0; i < clntCnt; i++)//접속을 종료한 클라이언트 소켓 목록에서 제거 처리
 	{
 		if (hClntSock == clntSocks[i])
 		{
@@ -95,21 +96,21 @@ unsigned WINAPI HandleClnt(void* arg)
 		}
 	}
 	clntCnt--;
-	ReleaseMutex(hMutex);
-	closesocket(hClntSock);
+	ReleaseMutex(hMutex);//뮤텍스 이용 종료
+	closesocket(hClntSock);//소켓 종료
 	return 0;
 }
 
-void SendMsg(char* msg, int len)
+void SendMsg(char* msg, int len)//메시지 전송함수
 {
 	int i;
-	WaitForSingleObject(hMutex, INFINITE);
+	WaitForSingleObject(hMutex, INFINITE);//메시지 전송 동기화를 위한 뮤텍스 대기
 	for (i = 0; i < clntCnt; i++)
-		send(clntSocks[i], msg, len, 0);
-	ReleaseMutex(hMutex);
+		send(clntSocks[i], msg, len, 0);//등록된 소켓들 모두에게 설정한 메시지 전달
+	ReleaseMutex(hMutex);//뮤텍스 이용 종료
 }
 
-void ErrorHandling(char* msg)
+void ErrorHandling(char* msg)//에러 표시용
 {
 	fputs(msg, stderr);
 	fputc('\n', stderr);
